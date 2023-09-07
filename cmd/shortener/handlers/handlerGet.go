1b9ea0bf49fedaadcb612448f6d4e8d2559b5186
petroHutro <artyrxx@list.ru>
2023-09-07 23:31:55 +0300

    iter3 fix hendkerGet

--- cmd/shortener/handlers/handlerGet.go
+++ cmd/shortener/handlers/handlerGet.go
@@ -3,14 +3,12 @@ package handlers
 import (
 	"net/http"
 	"sprint/cmd/shortener/db"
-
-	"github.com/go-chi/chi/v5"
 )
 
 func HandlerGet(w http.ResponseWriter, r *http.Request) {
-	// shortLink := r.URL.String()[1:]
+	shortLink := r.URL.String()[1:]
 	// shortLink := r.URL.Query().Get("id")
-	shortLink := chi.URLParam(r, "id")
+	// shortLink := chi.URLParam(r, "id")
 	if shortLink == "" {
 		w.WriteHeader(http.StatusBadRequest)
 		return
