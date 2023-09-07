d56439b210602daa7db5c7303b26cbc46efb6418
petroHutro <artyrxx@list.ru>
2023-09-07 23:16:53 +0300

    iter3 start

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,7 +4,8 @@ import (
 	"net/http"
 	"sprint/cmd/shortener/handlers"
 
-	"github.com/gorilla/mux"
+	// "github.com/gorilla/mux"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
@@ -14,8 +15,17 @@ func main() {
 }
 
 func run() error {
-	r := mux.NewRouter()
-	r.HandleFunc("/", handlers.HandlerPost).Methods("POST")
-	r.HandleFunc("/{id:[a-zA-Z0-9]+}", handlers.HandlerGet).Methods("GET")
+	r := chi.NewRouter()
+
+	r.Route("/", func(r chi.Router) {
+		r.Post("/", handlers.HandlerPost)
+	})
+	r.Route("/{id:[a-zA-Z0-9]+}", func(r chi.Router) {
+		r.Get("/", handlers.HandlerGet)
+	})
+
+	// r := mux.NewRouter()
+	// r.HandleFunc("/", handlers.HandlerPost).Methods("POST")
+	// r.HandleFunc("/{id:[a-zA-Z0-9]+}", handlers.HandlerGet).Methods("GET")
 	return http.ListenAndServe(`:8080`, r)
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module sprint
 go 1.19
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gorilla/mux v1.8.0
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.8.4
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
